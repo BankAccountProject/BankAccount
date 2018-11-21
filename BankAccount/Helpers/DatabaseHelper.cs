27620c5db6cdafcb964e64a57ef15bcd5d8e5728
unknown <aleksandra.owcz@vp.pl>
2018-11-21 22:39:58 +0100

    tests

---
+++ BankAccount/Helpers/DatabaseHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BankAccount.Helpers
+{
+    public static class DatabaseHelper
+    {
+        public static bool PasswordAreEqual(string password, string confirmPassword)
+        {
+            if (password == confirmPassword)
+                return true;
+            return false;
+        }
+
+        public static string Generate26DigitAccountNumber()
+        {
+            Random generator = new Random();
+            string accountNumber = generator.Next(100000, 999999).ToString();
+            for (int i = 0; i < 3; ++i)
+            {
+                accountNumber += generator.Next(100000, 999999).ToString();
+            }
+            return accountNumber;
+        }
+
+        public static bool CheckNumberOfDigits(string accountNumber)
+        {
+            // delete whitespace
+            accountNumber = Regex.Replace(accountNumber, @"\s+", "");
+            if (accountNumber.ToCharArray().Count() == 26)
+                return true;
+            return false;
+        }
+
+        public static bool ValidatePassword(string password)
+        {
+            // a-z V A-Z, 0-9, symbol-ASCII
+            bool result = password.Any(c => char.IsLetter(c)) &&
+                          password.Any(c => char.IsDigit(c)) &&
+                          password.Any(c => char.IsSymbol(c));
+            return result;
+        }
+
+        public static bool CheckBankAccountIfNumber(string accountNumber)
+        {
+            accountNumber = Regex.Replace(accountNumber, @"\s+", "");
+            return accountNumber.All(Char.IsDigit);
+        }
+    }
+}
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,76 @@
+using BankAccount.Helpers;
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("password", "password")]
+        [InlineData("password3", "password3")]
+        public void Passing_Passwords_Are_Equal(string pass, string confPass)
+        {
+            // Arrange
+            string password = pass;
+            string confirmPassword = confPass;
+
+            // Act
+            bool result = DatabaseHelper.PasswordAreEqual(password, confirmPassword);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("2000 0001 2345 6789 1234 4550")]
+        [InlineData("200000012345678912344550")]
+        public void Passing_Check_Number_Of_Digits(string accountNumber)
+        {
+            // Act
+            bool result = DatabaseHelper.CheckNumberOfDigits(accountNumber);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("2000 0001 2345 6789 1234 4550")]
+        [InlineData("200000012345678912344550")]
+        public void Passing_Check_Bank_Account_If_Number(string accountNumber)
+        {
+            // Act
+            bool result = DatabaseHelper.CheckBankAccountIfNumber(accountNumber);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("aododod90@")]
+        [InlineData("aAAAAd90@")]
+        [InlineData("aodod--==od90@")]
+        public void Passing_Validate_Password(string password)
+        {
+            // a-z V A-Z, 0-9, symbol-ASCII
+            // Act
+            bool result = DatabaseHelper.ValidatePassword(password);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Not_Passing_Check_Bank_Account_If_Number()
+        {
+            // Arrange 
+            string accountNumber = "2000 0001 2345 6789 1234 455g";
+
+            // Act
+            bool result = DatabaseHelper.CheckBankAccountIfNumber(accountNumber);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
